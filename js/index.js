822a8118e610e13aa561861e49e80e9e4603df43
Gus Baden <gusbaden96@gamil.com>
2019-12-03 13:59:48 -0800

    added some events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,53 @@
 // Your code goes here
+const logoBig = document.querySelector(".logo-heading")
+
+logoBig.addEventListener("mouseover", () => {
+    logoBig.style.transform = "scale(1.2)";
+    logoBig.style.transition = "transform .3s"
+})
+
+logoBig.addEventListener("mouseleave", () => {
+    logoBig.style.transform = "scale(1)"
+})
+
+
+const keys = document.querySelector(".intro img")
+
+keys.addEventListener("click", () => {
+    keys.style.transform = "scale(1.1)";
+    keys.style.transistion = "transform .3s"
+})
+
+keys.addEventListener("dblclick", () => {
+    keys.style.transform = "scale(1)";
+    keys.style.transition = "transform .2";
+})
+
+
+const scrollText = document.querySelector(".intro h2")
+
+window.addEventListener("scroll", () => {
+    scrollText.style.color = "red"
+    scrollText.style.transition = "transform .2";
+})
+
+
+
+const resizeImage =
+document.querySelector(".intro img");
+
+window.addEventListener('resize', () => {
+resizeImage.src = "https://c4.wallpaperflare.com/wallpaper/63/433/185/desert-truck-bus-fallout-wallpaper-preview.jpg"
+})
+
+
+
+
+const mapimg = document.querySelector(".text-content img")
+
+mapimg.addEventListener("drag", () => {
+    mapimg.style.transform= "scale(2)";
+})
+
+
+
