1a761ec11379a7ae74a3c9aec8c56986934d378e
Gus Baden <gusbaden96@gamil.com>
2019-12-03 15:44:19 -0800

    finished mvp

--- js/index.js
+++ js/index.js
@@ -1,16 +1,47 @@
 // Your code goes here
+
+//logo 
 const logoBig = document.querySelector(".logo-heading")
 
 logoBig.addEventListener("mouseover", () => {
     logoBig.style.transform = "scale(1.2)";
     logoBig.style.transition = "transform .3s"
+
+
 })
 
 logoBig.addEventListener("mouseleave", () => {
     logoBig.style.transform = "scale(1)"
 })
 
+const pressKey = document.querySelector(".logo-heading")
 
+window.addEventListener("keydown", () => {
+    pressKey.style.color = "red";
+})
+
+window.addEventListener("keyup", () => {
+    pressKey.style.color = "black";
+})
+
+window.addEventListener("keypress", (event) => {
+    pressKey.style.color = "blue";
+    event.stopPropagation();
+})
+
+
+//NAV
+
+const wheelVar = document.querySelector(".nav-link")
+
+window.addEventListener("wheel", () => {
+    wheelVar.style.color = "red";
+})
+
+
+
+
+//HEADER IMG
 const keys = document.querySelector(".intro img")
 
 keys.addEventListener("click", () => {
@@ -23,16 +54,6 @@ keys.addEventListener("dblclick", () => {
     keys.style.transition = "transform .2";
 })
 
-
-const scrollText = document.querySelector(".intro h2")
-
-window.addEventListener("scroll", () => {
-    scrollText.style.color = "red"
-    scrollText.style.transition = "transform .2";
-})
-
-
-
 const resizeImage =
 document.querySelector(".intro img");
 
@@ -41,13 +62,50 @@ resizeImage.src = "https://c4.wallpaperflare.com/wallpaper/63/433/185/desert-tru
 })
 
 
+//H2 
+const scrollText = document.querySelector(".intro h2")
 
-
-const mapimg = document.querySelector(".text-content img")
-
-mapimg.addEventListener("drag", () => {
-    mapimg.style.transform= "scale(2)";
+window.addEventListener("scroll", () => {
+    scrollText.style.color = "red"
+    scrollText.style.transition = "transform .2";
 })
+
+
+//Propagation 
+
+const props = document.querySelector(".content-destination")
+
+props.addEventListener("click", () => {
+    props.style.background = "red";
+   
+})
+
+const nextStep = document.querySelector('.content-destination h2');
+nextStep.addEventListener("click", (event) => {
+    nextStep.style.color = "blue";
+    event.stopPropagation();
+    console.log(event);
+})
+
+
+//Prevent Links from Refreshing
+const stoplink = document.querySelectorAll(".nav-link")
+
+stoplink.forEach(el => {
+    el.addEventListener("click", (event) => {
+        event.preventDefault();
+        console.log("stopped the link");
+    })
+})
+
+
+
+
+
+
+
+
+
 
 
 
